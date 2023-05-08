41154eabe0b65a70d6fed90f7afcfe2eb694ff5b
svenrudolph <39859138+svenrudolph@users.noreply.github.com>
2023-05-08 10:46:57 +0200

    remove old code

--- macros/insert_into_metadata_table.sql
+++ macros/insert_into_metadata_table.sql
@@ -13,15 +13,6 @@
     {% do run_query(insert_into_table_query) %}
 {%- endmacro %}
 
--- {% macro snowflake__insert_into_metadata_table(database_name, schema_name, table_name, content) -%}
---     {% set insert_into_table_query %}
---     insert into {{database_name}}.{{ schema_name }}.{{ table_name }}
---     {{ content }}
---     {% endset %}
-
---     {% do run_query(insert_into_table_query) %}
--- {%- endmacro %}
-
 {% macro snowflake__insert_into_metadata_table(database_name, schema_name, table_name, content) -%}
     {% set table_columns_query %}
         select column_name, data_type
