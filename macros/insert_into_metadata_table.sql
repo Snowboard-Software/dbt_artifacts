015f6a9f359d56ec9838ab71a5ea112162da6b48
svenrudolph <39859138+svenrudolph@users.noreply.github.com>
2023-05-08 10:43:16 +0200

    Fix syntax error

--- macros/insert_into_metadata_table.sql
+++ macros/insert_into_metadata_table.sql
@@ -23,47 +23,57 @@
 -- {%- endmacro %}
 
 {% macro snowflake__insert_into_metadata_table(database_name, schema_name, table_name, content) -%}
-{% set table_columns_query %}
-select column_name, data_type
-from information_schema.columns
-where table_catalog = '{{database_name}}'
-and table_schema = '{{schema_name}}'
-and table_name = '{{table_name}}'
-{% endset %}
-{% set table_columns = run_query(table_columns_query) %}
-
-{% set content_columns = content.split('\n')[0].split(',') %}
-{% set content_columns = [column.strip() for column in content_columns] %}
-
-{% set content_data_types = content.split('\n')[1].split(',') %}
-{% set content_data_types = [data_type.strip() for data_type in content_data_types] %}
-
-{% if len(table_columns) == len(content_columns) %}
-    {% set column_check = True %}
-    {% for index, column in enumerate(table_columns) %}
-        {% if column.column_name != content_columns[index] or column.data_type != content_data_types[index] %}
-            {% set column_check = False %}
-            {% break %}
-        {% endif %}
+    {% set table_columns_query %}
+        select column_name, data_type
+        from information_schema.columns
+        where table_catalog = '{{database_name}}'
+        and table_schema = '{{schema_name}}'
+        and table_name = '{{table_name}}'
+    {% endset %}
+    
+    {% set table_columns = run_query(table_columns_query) %}
+    
+    {% set content_columns = content.split('\n')[0].split(',') %}
+    {% set content_columns_list = [] %}
+    {% for column in content_columns %}
+        {% set column_trimmed = column.strip() %}
+        {% do content_columns_list.append(column_trimmed) %}
     {% endfor %}
     
-    {% if column_check %}
-        {% set insert_into_table_query %}
-            insert into {{database_name}}.{{ schema_name }}.{{ table_name }}
-            {{ content }}
-        {% endset %}
-        {% do run_query(insert_into_table_query) %}
+    {% set content_data_types = content.split('\n')[1].split(',') %}
+    {% set content_data_types_list = [] %}
+    {% for data_type in content_data_types %}
+        {% set data_type_trimmed = data_type.strip() %}
+        {% do content_data_types_list.append(data_type_trimmed) %}
+    {% endfor %}
+    
+    {% if len(table_columns) == len(content_columns_list) %}
+        {% set column_check = True %}
+        {% for index, column in enumerate(table_columns) %}
+            {% if column.column_name != content_columns_list[index] or column.data_type != content_data_types_list[index] %}
+                {% set column_check = False %}
+                {% break %}
+            {% endif %}
+        {% endfor %}
+        
+        {% if column_check %}
+            {% set insert_into_table_query %}
+                insert into {{database_name}}.{{ schema_name }}.{{ table_name }}
+                {{ content }}
+            {% endset %}
+            {% do run_query(insert_into_table_query) %}
+        {% else %}
+            {% set error_message = "The column names and/or data types in the content passed to the macro do not match the schema of the existing table." %}
+            {% do log(error_message) %}
+            {% do raise(error_message) %}
+        {% endif %}
     {% else %}
-        {% set error_message = "The column names and/or data types in the content passed to the macro do not match the schema of the existing table." %}
+        {% set error_message = "The number of columns in the content passed to the macro does not match the schema of the existing table." %}
         {% do log(error_message) %}
         {% do raise(error_message) %}
     {% endif %}
-{% else %}
-    {% set error_message = "The number of columns in the content passed to the macro does not match the schema of the existing table." %}
-    {% do log(error_message) %}
-    {% do raise(error_message) %}
-{% endif %}
 {%- endmacro %}
+
 
 
 {% macro bigquery__insert_into_metadata_table(database_name, schema_name, table_name, content) -%}
