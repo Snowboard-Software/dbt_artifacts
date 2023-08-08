d6cb9774c15c5d0c24ee4f31131ca6699254237e
Hasnain Yousuf - @codvlpr <hasnainyousuf.306@gmail.com>
2023-08-08 09:42:46 +0500

    not sending tests all_results

--- models/sources/tests.sql
+++ models/sources/tests.sql
@@ -13,7 +13,6 @@ select
     cast(null as {{ type_string() }}) as test_path,
     cast(null as {{ type_array() }}) as tags,
     cast(null as {{ type_string() }}) as compiled_code,
-    cast(null as {{ type_string() }}) as raw_code,
-    cast(null as {{ type_json() }}) as all_results
+    cast(null as {{ type_string() }}) as raw_code
 from dummy_cte
 where 1 = 0
